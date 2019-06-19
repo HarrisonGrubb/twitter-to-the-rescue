24dbc29f216405db4d7a1bb0ab7996f38fd5d6f5
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-19 19:55:36 -0400

    getting the twitters data :+1:
    able to create list of dicts based on parameters

--- twitter-rescue.py
+++ twitter-rescue.py
@@ -1,17 +1,11 @@
 import datetime
 import os
-import pprint
-
+from pprint import pprint
+import json
 from dotenv import load_dotenv
 import tweepy
 
 load_dotenv()
-
-CONSUMER_KEY = os.environ.get("TWITTER_API_KEY")
-CONSUMER_SECRET = os.environ.get("TWITTER_API_SECRET")
-ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-
 
 
 CONSUMER_KEY = os.environ.get("TWITTER_API_KEY")
@@ -32,6 +26,18 @@ client = tweepy.API(auth)
 
 user = client.me() # get information about the currently authenticated user
 
-data = client.user_timeline('NYCTSubway')
+data = client.user_timeline('NYCTSubway', count = 20,max_id = 1141476562350563329, exclude_replies = True , include_rts = False)
 
-print(data)
+tweet_list = []
+for t in range(0, len(data)):
+    tweet_list.append({
+        'text' : data[t].text,
+        'time' : data[t].created_at,
+        'id' : data[t].id})
+
+breakpoint()
+
+# convert to dict
+# data[0].__dict__
+#data[0].text accessing elements
+
