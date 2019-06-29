92d7bd02e9b0e69e28ecd3daa98ec7463839ef33
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-28 21:31:45 -0400

    Big readme update and cosmetic code changes

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# twitter-to-the-rescue
-My freestyle project using twitter to save my commute
+# Twitter to the Rescue
+----
+### Saving your commute one tweet at a time
+
+By parsing through the MTA's twitter account at scheduled times.  This project can give you an advanced notice subway delays.  
+
+## Setup
+
+You can clone this repo to your own Github page either through the web-based GUI or through GIT commands.
+
+After you've forked the repo ensure you download a local copy to your machine.  From there you can navigate to your repo via the below command.
+
+``` cd /you_repo_name ```
+
+From there you'll want to create a new virtual enviroment to install the necessary third party pacakges.  Follow the commands below if you need help setting up your virtual enviroment.
+
+ 
+```conda create -n name-of-your-env python = 3.7```
+```conda activate name-of-your-env```
+
+Now that you've got a working virtual enviroment you can use pip to install the third party packages via the command below.
+
+``` pip install -r requirments.txt```
+
+Finally, this program requires registering for three services: 
+* Twitter: source of the Tweets :)
+* Sendgrid: makes sending email notices easy
+* Heroku: to PAAS company that will allow you to schedule the program
+
+## Security Steps
+
+After signing up for Twitter, Sendgrid, and if you want to schedule the program Heroku.  You'll need to store these variables in a .env file.  In your directory you can run the following command.
+
+``` touch .env```
+
+Open that file and fill in the below variables with your own unique login credentials.
+
+* CONSUMER_KEY = your-key-here
+* CONSUMER_SECRET = your-key-here
+* ACCESS_TOKEN = your-key-here
+* ACCESS_TOKEN_SECRET = your-key-here
+* SENDGRID_API_KEY = your-key-here
+* MY_ADDRESS = your-email-address-here
+* SENDGRID_TEMPLATE_ID = name-of-the-sendgrid-template
+
+## About to run
+
+Before you can run this code you'll want to make sure you have a copy of the sendgrid email template saved into your sendgrid profile.  I've included a copy of mine with this repo.
+
+Additionally in line 108 of the twitter-rescue.py you'll need to update your own train schedule to make it relevant for you.  If you made any adjustments look for the data named user_travel.  
+
+Navigate through the nested dictionary replacing my name with yours.  The day index is listed below so you can make sure the days you care about are included in the data.  
+
+* 0 = Monday
+* 1 = Tuesday
+* 2 = Wednesday
+* 3 = Thursday
+* 4 = Friday
+* 5 = Saturday
+* 7 = Sunday
+
+Additionally, if you take a train with letters in it, for example the A Train, include both directions (example below).  This is particularly relevant for people who take the A Train.  The program will pick up every instance of the following passage as a delay: "6 trains are running express after **a train's** brakes were activated".  
+
+* This is good: "Northbound E", "Southbound E"
+* This will have too many false positives: "E train"
+
+If you'd like for the tweets you've captured to be stored locally you can uncomment the code after the comment writing data to file.  
--- twitter-rescue.py
+++ twitter-rescue.py
@@ -6,10 +6,11 @@ from dotenv import load_dotenv
 import tweepy
 import pandas as pd
 import pytz
-from dateutil import tz
 import sendgrid
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+
+
 load_dotenv()
 
 
@@ -19,10 +20,11 @@ ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
 MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
-sgclient = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
 SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID")
+
 # AUTHENTICATE
 
+sgclient = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 sg = sendgrid.SendGridAPIClient(SENDGRID_API_KEY)
@@ -40,7 +42,7 @@ data = client.user_timeline('NYCTSubway', exclude_replies = True , include_rts =
 #timezone fix
 utc = pytz.timezone('UTC')
 
-#function for later
+#function to email out the delays
 def email_out(reciever, delays):
         template_data = {
                 "firstName" : reciever,
@@ -92,15 +94,6 @@ while len(tweet_list) < 20:
 
 
 
-# all totally useless changing the time so far
-# [ts['time'].to('UTC') for ts in tweet_list]
-
-
-# [datetime.strptime(str(ts['time']), "%Y-%m-%d %H:%M:%S") for ts in tweet_list]
-# [ts['time'].replace(tzinfo = timezone('UTC')) for ts in tweet_list]
-
-# [print(type(ts['time'])) for ts in tweet_list]
-# [print(ts['time'].strftime("%Y-%m-%d %H:%M:%S %Z%z")) for ts in tweet_list]
 
 
 user_travel = [{'user' : 'harrison', 'commutes' : {
@@ -118,7 +111,7 @@ today = str(datetime.now().weekday())
 users_to_email = []
 list_count = -1
 
-
+# finding relevant train alerts in the list of tweets
 for user in user_travel:
         if today not in list(user['commutes'].keys()):
                 pass
@@ -130,7 +123,7 @@ for user in user_travel:
                                         users_to_email.append({'user' : user['user'], 'tweet_text' : []})
                                         users_to_email[list_count]['tweet_text'].append(twit['text'])
 
-
+#mailing the user their notifications
 for recipient in users_to_email:
         reciever = recipient['user']
         delays = recipient['tweet_text']
@@ -153,35 +146,26 @@ for recipient in users_to_email:
 
 # tweet_data.to_csv(csv_file_path, index= False)
 
+# Help attribution
+
 #https://www.afternerd.com/blog/python-string-contains/
 # timezone question
 #https://stackoverflow.com/questions/6935225/twitter-time-date-stamp-which-time-zone-is-it
-
-
-# convert to dict
-# data[0].__dict__
-#data[0].text accessing elements
-
+# heroku deploy adapted from Prof Rossetti
 
 ###### ROADMAP
 
+#Parse time of tweets to ensure that they're relevant
 
-# 3.) parse time of tweets to ensure that they're relevant
+# [ts['time'].to('UTC') for ts in tweet_list]
 
-# Fix template at this point 
+
+# [datetime.strptime(str(ts['time']), "%Y-%m-%d %H:%M:%S") for ts in tweet_list]
+# [ts['time'].replace(tzinfo = timezone('UTC')) for ts in tweet_list]
+
+# [print(type(ts['time'])) for ts in tweet_list]
+# [print(ts['time'].strftime("%Y-%m-%d %H:%M:%S %Z%z")) for ts in tweet_list]
 
 limit = client.rate_limit_status()
 print(limit['resources']['statuses']['/statuses/user_timeline'])
 
-# breakpoint()
-
-
-# for twit in tweet_list:
-#         for user in user_travel:
-#                 if today not in list(user['commutes'].keys()):
-#                         pass
-#                 else:
-#                         for commute in user['commutes'][today]:
-#                                 if commute in twit['text']and any(delay in twit['text'] for delay in delay_word):
-#                                         users_to_email.append({'user' : user['user'], 'tweet_text' : twit['text']})
-# ^  original for loop to find compare travelers and delay tweets works but is jenky
